255f22b1e72bc8c7642c9b62aea237ebfbb0ffa1
Stefan Bachmann <st.lebach@gmail.com>
2013-02-19 04:12:17 -0700

    More SpeedTest.

--- ashley-tests/src/ashley/tests/SpeedTest.java
+++ ashley-tests/src/ashley/tests/SpeedTest.java
@@ -9,6 +9,8 @@ import ashley.tests.utils.Timer;
 import ashley.utils.Array;
 
 public class SpeedTest {
+	public static int NUMBER_ENTITIES = 10000;
+	
 	public static void main(String[] args){
 		Timer timer = new Timer();
 		Array<Entity> entities = new Array<>();
@@ -17,10 +19,16 @@ public class SpeedTest {
 		
 		engine.addSystem(new MovementSystem());
 		
+		System.out.println("Number of entities: " + NUMBER_ENTITIES);
+		
+		/** Adding entities */
 		timer.start("entities");
 		
-		for(int i=0; i<10000; i++){
+		entities.ensureCapacity(NUMBER_ENTITIES);
+		
+		for(int i=0; i<NUMBER_ENTITIES; i++){
 			Entity entity = new Entity();
+			engine.addEntity(entity);
 			entity.add(new MovementComponent(10, 10));
 			entity.add(new PositionComponent(0, 0));
 			
@@ -28,5 +36,23 @@ public class SpeedTest {
 		}
 		
 		System.out.println("Entities added time: " + timer.stop("entities") + "ms");
+		
+		/** Removing components */
+		timer.start("componentRemoved");
+		
+		for(Entity e:entities){
+			e.remove(PositionComponent.class);
+		}
+		
+		System.out.println("Component removed time: " + timer.stop("componentRemoved") + "ms");
+		
+		/** Removing entities */
+		timer.start("entitiesRemoved");
+		
+		for(Entity e:entities){
+			engine.removeEntity(e);
+		}
+		
+		System.out.println("Entity removed time: " + timer.stop("entitiesRemoved") + "ms");
 	}
 }
