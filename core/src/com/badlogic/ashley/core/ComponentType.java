8b48cca9d3f951bb2017b0e53222d375039958ca
David Saltares <david.saltares@gmail.com>
2014-07-01 09:27:55 +0100

    Simplifies some hashCode() methods

--- core/src/com/badlogic/ashley/core/ComponentType.java
+++ core/src/com/badlogic/ashley/core/ComponentType.java
@@ -90,10 +90,7 @@ public class ComponentType {
     
     @Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + index;
-		return result;
+		return index;
 	}
 
 	@Override
--- core/src/com/badlogic/ashley/core/Entity.java
+++ core/src/com/badlogic/ashley/core/Entity.java
@@ -178,10 +178,7 @@ public class Entity {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + index;
-		return result;
+		return index;
 	}
 
 	@Override
