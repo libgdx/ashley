1f800e95009cf2dd734c49f1fd8cb1fb9f8ae08b
Andres Araujo <a.araujo.azua@gmail.com>
2014-08-03 16:41:19 -0500

    chore: clean up

--- ashley/src/com/badlogic/ashley/fsm/IComponentProvider.java
+++ ashley/src/com/badlogic/ashley/fsm/IComponentProvider.java
@@ -2,10 +2,6 @@ package com.badlogic.ashley.fsm;
 
 import com.badlogic.ashley.core.Component;
 
-/**
- * User: andres
- * Date: 7/3/14 - 4:55 PM
- */
 public interface IComponentProvider<T extends Component> {
 
     public T getComponent();
