569f089f9236dc8c2da09f4fe3b4d33880745d8a
David Saltares <david.saltares@gmail.com>
2014-08-10 08:29:16 +0100

    PooledEngine has configurable pools and no longer has a maximum component count of 100

--- ashley/src/com/badlogic/ashley/core/PooledEngine.java
+++ ashley/src/com/badlogic/ashley/core/PooledEngine.java
@@ -16,9 +16,11 @@
 
 package com.badlogic.ashley.core;
 
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pool.Poolable;
-import com.badlogic.gdx.utils.Pools;
+import com.badlogic.gdx.utils.ReflectionPool;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 
 
@@ -37,11 +39,32 @@ import com.badlogic.gdx.utils.reflect.ClassReflection;
 public class PooledEngine extends Engine {
 	
 	private EntityPool entityPool;
+	private ComponentPools componentPools;
 	
+	/**
+	 * Creates a new PooledEngine with a maximum of 100 entities and 100 components of each type.
+	 * Use {@link #PooledEngine(int, int, int, int)} to configure the entity and component pools.
+	 */
 	public PooledEngine() {
+		this(10, 100, 10, 100);
+	}
+	
+	/**
+	 * Creates new PooledEngine with the specified pools size configurations. 
+	 *  
+	 * @param entityPoolInitialSize initial number of pre-allocated entities.
+	 * @param entityPoolMaxSize maximum number of pooled entities.
+	 * @param componentPoolInitialSize initial size for each component type pool.
+	 * @param componentPoolMaxSize maximum size for each component type pool.
+	 */
+	public PooledEngine(int entityPoolInitialSize,
+						int entityPoolMaxSize,
+						int componentPoolInitialSize,
+						int componentPoolMaxSize) {
 		super();
 		
-		entityPool = new EntityPool();
+		entityPool = new EntityPool(entityPoolInitialSize, entityPoolMaxSize);
+		componentPools = new ComponentPools(componentPoolInitialSize, componentPoolMaxSize);
 	}
 	
 	/**
@@ -69,7 +92,7 @@ public class PooledEngine extends Engine {
 	 * pool whenever it's removed from an {@link Entity} or the {@link Entity} itself it's removed.
 	 */
 	public <T extends Component> T createComponent(Class<T> componentType) {
-		return Pools.obtain(componentType);
+		return componentPools.obtain(componentType);
 	}
 	
 	private class PooledEntity extends Entity implements Poolable {
@@ -77,7 +100,7 @@ public class PooledEngine extends Engine {
 		@Override
 		public Component remove(Class<? extends Component> componentType){
 			Component component = super.remove(componentType);
-			Pools.free(component);
+			componentPools.free(component);
 			return component;
 		}
 
@@ -90,9 +113,60 @@ public class PooledEngine extends Engine {
 	
 	private class EntityPool extends Pool<PooledEntity> {
 
+		public EntityPool(int initialSize, int maxSize) {
+			super(initialSize, maxSize);
+		}
+		
 		@Override
 		protected PooledEntity newObject() {
 			return new PooledEntity();
+		}
+	}
+	
+	private class ComponentPools {
+		private ObjectMap<Class<?>, ReflectionPool> pools;
+		private int initialSize;
+		private int maxSize;
+		
+		public ComponentPools(int initialSize, int maxSize) {
+			this.pools = new ObjectMap<Class<?>, ReflectionPool>();
+			this.initialSize = 0;
+			this.maxSize = 0;
+		}
+		
+		public <T> T obtain(Class<T> type) {
+			ReflectionPool pool = pools.get(type);
+			
+			if (pool == null) {
+				pool = new ReflectionPool(type, initialSize, maxSize);
+				pools.put(type, pool);
+			}
+			
+			return (T)pool.obtain();
+		}
+		
+		public void free(Object object) {
+			if (object == null) {
+				throw new IllegalArgumentException("object cannot be null.");
+			}
+			
+			ReflectionPool pool = pools.get(object.getClass());
+			
+			if (pool == null) {
+				return; // Ignore freeing an object that was never retained.
+			}
+			
+			pool.free(object);
+		}
+
+		public void freeAll(Array objects) {
+			if (objects == null) throw new IllegalArgumentException("objects cannot be null.");
+			
+			for (int i = 0, n = objects.size; i < n; i++) {
+				Object object = objects.get(i);
+				if (object == null) continue;
+				free(object);
+			}
 		}
 	}
 }
