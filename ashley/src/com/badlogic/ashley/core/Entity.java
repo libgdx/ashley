8140c36d05167a76a07dcc86bc8e60cf336e6995
Lusito <core@roughael.net>
2014-10-12 21:26:49 +0200

    Recreate unique id on PooledEngine.createEntity

--- ashley/src/com/badlogic/ashley/core/Entity.java
+++ ashley/src/com/badlogic/ashley/core/Entity.java
@@ -79,6 +79,15 @@ public class Entity {
 	}
 	
 	/**
+	 * Internal use.
+	 * 
+	 * Creates a unique id
+	 */
+	void createId() {
+		uuid = new Long(nextId++);
+	}
+	
+	/**
 	 * Adds a {@link Component} to this Entity. If a {@link Component} of the same type already exists, it'll be replaced.
 	 * @return The Entity for easy chaining
 	 */
