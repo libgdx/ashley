bb188e1d373aaf1e6270dcddf3c52e0401d33ed3
jopa <jopa@WITLAP-JOPA.wit.local>
2014-10-21 15:14:18 +0200

    Implemented Iterable Interface for ImmutableArray Implemented Collection Interface for Bag Adapted IntervalIteratingSystem and IteratingSystem to reflect the changes above Added new Test class for ImmutableArray

--- ashley/src/com/badlogic/ashley/systems/IteratingSystem.java
+++ ashley/src/com/badlogic/ashley/systems/IteratingSystem.java
@@ -1,19 +1,20 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright 2014 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *****************************************************************************
+ */
 package com.badlogic.ashley.systems;
 
 import com.badlogic.ashley.core.Engine;
@@ -23,67 +24,79 @@ import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
 
 /**
- * A simple EntitySystem that iterates over each entity and calls processEntity() for each entity every time
- * the EntitySystem is updated. This is really just a convenience class as most systems iterate over a list
- * of entities.
- * 
+ * A simple EntitySystem that iterates over each entity and calls
+ * processEntity() for each entity every time the EntitySystem is updated. This
+ * is really just a convenience class as most systems iterate over a list of
+ * entities.
+ *
  * @author Stefan Bachmann
  */
 public abstract class IteratingSystem extends EntitySystem {
-	/** The family describing this systems entities */
-	private Family family;
-	/** The entities used by this system */
-	private ImmutableArray<Entity> entities;
-	
-	/**
-	 * Instantiates a system that will iterate over the entities described by the Family.
-	 * @param family The family of entities iterated over in this System
-	 */
-	public IteratingSystem(Family family){
-		this(family, 0);
-	}
-	
-	/**
-	 * Instantiates a system that will iterate over the entities described by the Family, with a 
-	 * specific priority.
-	 * @param family The family of entities iterated over in this System
-	 * @param priority The priority to execute this system with (lower means higher priority)
-	 */
-	public IteratingSystem(Family family, int priority){
-		super(priority);
-		
-		this.family = family;
-	}
-		
-	@Override
-	public void addedToEngine(Engine engine) {
-		entities = engine.getEntitiesFor(family);
-	}
 
-	@Override
-	public void removedFromEngine(Engine engine) {
-		entities = null;
-	}
+    /**
+     * The family describing this systems entities
+     */
+    private Family family;
+    /**
+     * The entities used by this system
+     */
+    private ImmutableArray<Entity> entities;
 
-	@Override
-	public void update(float deltaTime) {
-		for (int i = 0; i < entities.size(); ++i) {
-			processEntity(entities.get(i), deltaTime);
-		}
-	}
-	
-	/**
-	 * @return set of entities processed by the system
-	 */
-	public ImmutableArray<Entity> getEntities() {
-		return entities;
-	}
+    /**
+     * Instantiates a system that will iterate over the entities described by
+     * the Family.
+     *
+     * @param family The family of entities iterated over in this System
+     */
+    public IteratingSystem(Family family) {
+        this(family, 0);
+    }
 
-	/**
-	 * This method is called on every entity on every update call of the EntitySystem. Override this to implement
-	 * your system's specific processing.
-	 * @param entity The current Entity being processed
-	 * @param deltaTime The delta time between the last and current frame
-	 */
-	protected abstract void processEntity(Entity entity, float deltaTime);
+    /**
+     * Instantiates a system that will iterate over the entities described by
+     * the Family, with a specific priority.
+     *
+     * @param family The family of entities iterated over in this System
+     * @param priority The priority to execute this system with (lower means
+     * higher priority)
+     */
+    public IteratingSystem(Family family, int priority) {
+        super(priority);
+
+        this.family = family;
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        entities = engine.getEntitiesFor(family);
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+        entities = null;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        for (Entity e : entities) {
+            processEntity(e, deltaTime);
+        }
+    }
+
+    /**
+     * @return set of entities processed by the system
+     */
+    public ImmutableArray<Entity> getEntities() {
+        return entities;
+    }
+
+    /**
+     * This method is called on every entity on every update call of the
+     * EntitySystem. Override this to implement your system's specific
+     * processing.
+     *
+     * @param entity The current Entity being processed
+     * @param deltaTime The delta time between the last and current frame
+     */
+    protected abstract void processEntity(Entity entity, float deltaTime);
 }
--- tests/src/com/badlogic/ashley/tests/BasicTest.java
+++ tests/src/com/badlogic/ashley/tests/BasicTest.java
@@ -1,19 +1,20 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright 2014 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *****************************************************************************
+ */
 package com.badlogic.ashley.tests;
 
 import com.badlogic.ashley.core.ComponentMapper;
@@ -28,106 +29,118 @@ import com.badlogic.ashley.tests.components.PositionComponent;
 import com.badlogic.ashley.utils.ImmutableArray;
 
 public class BasicTest {
-	
-	public static void main(String[] args){
-		PooledEngine engine = new PooledEngine();
-		
-		MovementSystem movementSystem = new MovementSystem();
-		PositionSystem positionSystem = new PositionSystem();
-		
-		engine.addSystem(movementSystem);
-		engine.addSystem(positionSystem);
-		
-		Listener listener = new Listener();
-		engine.addEntityListener(listener);
-		
-		for(int i=0; i<10; i++){
-			Entity entity = engine.createEntity();
-			entity.add(new PositionComponent(10, 0));
-			if(i > 5)
-				entity.add(new MovementComponent(10, 2));
-			
-			engine.addEntity(entity);
-		}
-		
-		log("MovementSystem has: " + movementSystem.entities.size() + " entities.");
-		log("PositionSystem has: " + positionSystem.entities.size() + " entities.");
-		
-		for(int i=0; i<10; i++){
-			engine.update(0.25f);
-			
-			if(i > 5)
-				engine.removeSystem(movementSystem);
-		}
-		
-		engine.removeEntityListener(listener);
-	}
-	
-	public static class PositionSystem extends EntitySystem {
-		public ImmutableArray<Entity> entities;
 
-		@Override
-		public void addedToEngine(Engine engine) {
-			entities = engine.getEntitiesFor(Family.getFor(PositionComponent.class));
-			log("PositionSystem added to engine.");
-		}
+    public static void main(String[] args) {
+        PooledEngine engine = new PooledEngine();
 
-		@Override
-		public void removedFromEngine(Engine engine) {
-			log("PositionSystem removed from engine.");
-			entities = null;
-		}
-	}
-	
-	public static class MovementSystem extends EntitySystem {
-		public ImmutableArray<Entity> entities;
-		
-		private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
-		private ComponentMapper<MovementComponent> mm = ComponentMapper.getFor(MovementComponent.class);
+        MovementSystem movementSystem = new MovementSystem();
+        PositionSystem positionSystem = new PositionSystem();
 
-		@Override
-		public void addedToEngine(Engine engine) {
-			entities = engine.getEntitiesFor(Family.getFor(PositionComponent.class, MovementComponent.class));
-			log("MovementSystem added to engine.");
-		}
+        engine.addSystem(movementSystem);
+        engine.addSystem(positionSystem);
 
-		@Override
-		public void removedFromEngine(Engine engine) {
-			log("MovementSystem removed from engine.");
-			entities = null;
-		}
+        Listener listener = new Listener();
+        engine.addEntityListener(listener);
 
-		@Override
-		public void update(float deltaTime) {
-			
-			for (int i = 0; i < entities.size(); ++i){
-				Entity e = entities.get(i);
-				
-				PositionComponent p = pm.get(e);
-				MovementComponent m = mm.get(e);
-				
-				p.x += m.velocityX * deltaTime;
-				p.y += m.velocityY * deltaTime;
-			}
-			
-			log(entities.size() + " Entities updated in MovementSystem.");
-		}
-	}
-	
-	public static class Listener implements EntityListener {
+        for (int i = 0; i < 10; i++) {
+            Entity entity = engine.createEntity();
+            entity.add(new PositionComponent(10, 0));
+            if (i > 5) {
+                entity.add(new MovementComponent(10, 2));
+            }
 
-		@Override
-		public void entityAdded(Entity entity) {
-			log("Entity added " + entity);
-		}
+            engine.addEntity(entity);
+        }
 
-		@Override
-		public void entityRemoved(Entity entity) {
-			log("Entity removed " + entity);
-		}
-	}
-	
-	public static void log(String string){
-		System.out.println(string);
-	}
+        log("MovementSystem has: " + movementSystem.entities.size() + " entities.");
+        log("PositionSystem has: " + positionSystem.entities.size() + " entities.");
+
+        for (int i = 0; i < 10; i++) {
+            engine.update(0.25f);
+
+            if (i > 5) {
+                engine.removeSystem(movementSystem);
+            }
+        }
+
+        engine.removeEntityListener(listener);
+    }
+
+    public static class PositionSystem extends EntitySystem {
+
+        public ImmutableArray<Entity> entities;
+
+        @Override
+        public void addedToEngine(Engine engine) {
+            entities = engine.getEntitiesFor(Family.getFor(PositionComponent.class));
+            log("PositionSystem added to engine.");
+        }
+
+        @Override
+        public void removedFromEngine(Engine engine) {
+            log("PositionSystem removed from engine.");
+            entities = null;
+        }
+    }
+
+    public static class MovementSystem extends EntitySystem {
+
+        public ImmutableArray<Entity> entities;
+
+        private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+        private ComponentMapper<MovementComponent> mm = ComponentMapper.getFor(MovementComponent.class);
+
+        @Override
+        public void addedToEngine(Engine engine) {
+            entities = engine.getEntitiesFor(Family.getFor(PositionComponent.class, MovementComponent.class));
+            log("MovementSystem added to engine.");
+        }
+
+        @Override
+        public void removedFromEngine(Engine engine) {
+            log("MovementSystem removed from engine.");
+            entities = null;
+        }
+
+        @Override
+        public void update(float deltaTime) {
+
+            for (Entity e : entities) {
+                PositionComponent p = pm.get(e);
+                MovementComponent m = mm.get(e);
+
+                p.x += m.velocityX * deltaTime;
+                p.y += m.velocityY * deltaTime;
+            }
+//
+//			for (int i = 0; i < entities.size(); ++i){
+//				Entity e = entities.get(i);
+//
+//				PositionComponent p = pm.get(e);
+//				MovementComponent m = mm.get(e);
+//
+//				p.x += m.velocityX * deltaTime;
+//				p.y += m.velocityY * deltaTime;
+//			}
+//			
+            log(entities.size() + " Entities updated in MovementSystem.");
+        }
+    }
+
+    public static class Listener implements EntityListener {
+
+        @Override
+        public void entityAdded(Entity entity) {
+            log("Entity added " + entity);
+        }
+
+        @Override
+        public void entityRemoved(Entity entity) {
+            log("Entity removed " + entity);
+        }
+    }
+
+    public static void log(String string) {
+        System.out.println(string);
+    }
 }
