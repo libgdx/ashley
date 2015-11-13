4449813f9952bd1bf148be32092892eed6d73947
Brent De Weerdt <deweerdt.brent@gmail.com>
2015-11-13 19:07:21 +0100

    Expose the interval in IntervalSystem
    This change makes it possible for subclasses to access the interval
    directly, without having to create a custom constructor.

--- ashley/src/com/badlogic/ashley/systems/IntervalSystem.java
+++ ashley/src/com/badlogic/ashley/systems/IntervalSystem.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,6 +42,10 @@ public abstract class IntervalSystem extends EntitySystem {
 		super(priority);
 		this.interval = interval;
 		this.accumulator = 0;
+	}
+
+	public float getInterval() {
+		return interval;
 	}
 
 	@Override
